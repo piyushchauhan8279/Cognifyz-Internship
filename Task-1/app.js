c3efe3c51a0d2a375d4bcd6417f97cf3821e15de
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-07-14 22:55:30 +0530

    add

--- Task-1/app.js
+++ Task-1/app.js
@@ -1,8 +1,12 @@
+// external modules
 const express=require('express')
-const PORT=3000
-
 const app=express()
 
+const bodyparser=require('body-parser')
+app.use(bodyparser.urlencoded)
+
+
+const PORT=3000
 app.listen(PORT,()=>{
   console.log(`Server is Listening at PORT:- ${PORT}`);
 })
